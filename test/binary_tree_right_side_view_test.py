4d9788476988190cd13323a10c6527a12a9994d4
jandk008 <zczhou10@gmail.com>
2019-07-31 22:48:00 +0900

    Create binary tree nodes in parametrize annotation

--- test/binary_tree_right_side_view_test.py
+++ test/binary_tree_right_side_view_test.py
@@ -9,8 +9,8 @@ def solution():
     return Solution()
 
 
-@pytest.mark.parametrize("nums, expected",
-                         [([1, 2, 3, None, 5, None, 4], [1, 3, 4]),
-                          ([1, 2, 3, 5, None, 4, None, 6], [1, 3, 4, 6])])
-def test_binary_tree_right_side_view(solution, nums, expected):
-    assert solution.rightSideView(TreeNode.fromlist(nums)) == expected
+@pytest.mark.parametrize("root, expected", [
+    (TreeNode.fromlist([1, 2, 3, None, 5, None, 4]), [1, 3, 4]),
+    (TreeNode.fromlist([1, 2, 3, 5, None, 4, None, 6]), [1, 3, 4, 6])])
+def test_binary_tree_right_side_view(solution, root, expected):
+    assert solution.rightSideView(root) == expected
