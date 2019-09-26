8834693e9c503b600854568e8d3a204b2bf6211e
jandk008 <zczhou10@gmail.com>
2019-09-26 16:44:46 +0900

    Refine test cases for is_subsequence problem

--- test/is_subsequence_test.py
+++ test/is_subsequence_test.py
@@ -8,12 +8,15 @@ def solution():
     return Solution()
 
 
-@pytest.mark.parametrize('s, t, expect',
-                         [('abc', 'abc', True), ('ab', 'ajib', True),
-                          ('ab', 'axiu', False), ('', '', True),
-                          ('', 'ji', True), ('ji', '', False),
-                          ('sfjib', 'sjfsfib', False),
-                          ('aib', 'scabubifbsf', True), ('ab', 'ssfaxab', True),
-                          ('a', 'biji', False)])
-def test_is_subsequence(solution: Solution, s: str, t: str, expect: bool):
-    assert solution.is_subsequence(s, t) == expect
+@pytest.mark.parametrize('s, t',
+                         [('abc', 'abc'), ('ab', 'ajib'), ('', ''), ('', 'ji'),
+                          ('aib', 'scabubifbsf'), ('ab', 'ssfaxab')])
+def test_is_subsequence(solution: Solution, s: str, t: str):
+    assert solution.is_subsequence(s, t) is True
+
+
+@pytest.mark.parametrize('s, t',
+                         [('ab', 'axiu'), ('ji', ''), ('sfjib', 'sjfsfib'),
+                          ('a', 'biji')])
+def test_is_not_subsequence(solution: Solution, s: str, t: str):
+    assert solution.is_subsequence(s, t) is False
