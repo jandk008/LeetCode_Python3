63dfb00cc75b433c6e80a9cc9ee5f568f57af057
jandk008 <zczhou10@gmail.com>
2020-06-29 00:52:30 +0900

    Fix out of bound bugs on Manacher algorithm

--- leetcode/manacher_longest_palindrome.py
+++ leetcode/manacher_longest_palindrome.py
@@ -5,20 +5,18 @@ class Solution:
         n = 2 * len(s) + 1
         lps = [0] * n
         lps[1] = 1
-        max_index, max_length, center, i = 1, 3, 1, 2
-        while i < n:
+        max_index, max_length, center, i = 1, 1, 1, 2
+        for i in range(n):
             left, right = 2 * center - i, center + lps[center]
-            length = lps[left]
-            if lps[left] >= right - i:
+            length = lps[left] if left >= 0 else 0
+            if i + length >= right:
                 length = right - i
                 l, r = i - length - 1, i + length + 1
                 while l >= 0 and r < n and (l % 2 == 0 or s[l // 2] == s[r // 2]):
-                    length, l, r = length + 2, l - 1, r + 1
-            lps[i] = length
-            if length > max_length:
-                max_index, max_length = i, lps[i]
-            i += 1
-            if i > right:
+                    length, l, r = length + 1, l - 1, r + 1
                 center = i
-        start = (max_index - max_length // 2) // 2
-        return s[start: start + max_length // 2]
+                if length > max_length:
+                    max_index, max_length = i, length
+            lps[i] = length
+        start = (max_index - max_length) // 2
+        return s[start: start + max_length]
